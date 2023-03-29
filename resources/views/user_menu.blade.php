fc7a42e12d2a1986802f0dfa63a680e60eb40d4c
yuichirotanaka <tanaka123305@icloud.com>
2023-03-29 23:35:34 +0900

    2023/03/29

--- resources/views/user_menu.blade.php
+++ resources/views/user_menu.blade.php
@@ -6,12 +6,12 @@
     </x-slot>
 
 <div class="container text-center">
-    <div class="d-flex flex-column align-items-center">
-        <a class="btn btn-danger btn-lg my-3" href="{{ route('kaitori') }}">買取査定</a>
-        <a class="btn btn-success btn-lg my-3" href="{{ route('pro_list') }}">商品管理</a>
+    <div class="d-flex flex-column align-items-center mt-5">
+        <a class="btn btn-danger btn-lg my-4"  href="{{ route('kaitori') }}">買取査定</a>
+        <a class="btn btn-success btn-lg my-4" href="{{ route('pro_list') }}">商品管理</a>
         <form method="POST" action="{{ route('logout') }}">
             @csrf
-            <button class="btn btn-secondary btn-lg my-3" type="submit">ログアウト</button>
+            <button class="btn btn-secondary btn-lg my-4" type="submit">ログアウト</button>
         </form>
     </div>
 </div>
